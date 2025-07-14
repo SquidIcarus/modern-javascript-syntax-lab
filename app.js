3901ce762c8b8dd047a946bff0a8c44e09061541
User Name <landonisaiah@gmail.com>
2025-07-14 12:27:17 -0700

    exercise 2 and 3 complete

--- app.js
+++ app.js
@@ -13,9 +13,53 @@ const nums = [13, 87, 2, 89, 12, 4, 90, 63];
 
 // Your code here:
 
-const numsTimesTwo = nums.map(function (num) {     
-        return num * 2;
+const numsTimesTwo = nums.map(function (num) {
+    return num * 2;
 });
 
+console.log("*** EXERCISE 1 ***")
 console.log("original array:", nums);
 console.log("iterated array:", numsTimesTwo);
+
+// ! Exercise 2:
+// a. Given the provided `pizzaToppings` array, use destructuring to extract 
+//    the first and second values and place them into variables. Name the 
+//    variable that corresponds to the first value `firstIngredient`. Name the 
+//    variable that corresponds to the second value `secondIngredient`.
+//
+// b. Console log the `firstIngredient` and `secondIngredient` variables.
+//
+// Starting code (don't modify this):
+
+const pizzaToppings = ['Pineapple', 'Olives', 'Anchovies'];
+
+// Your code here:
+
+const [firstIngredient, secondIngredient] = pizzaToppings;
+
+console.log("*** EXERCISE 2 ***")
+console.log(firstIngredient);
+console.log(secondIngredient);
+
+
+// ! Exercise 3:
+// a. Given the provided `car` object, use destructuring to create two
+//    variables: `make` and `model` that will hold the respective values.
+//
+// b. Console log the `make` and `model` variables.
+//
+// Starting code (don't modify this):
+
+const car = {
+    make: 'Audi',
+    model: 'Q5',
+};
+
+// Your code here:
+
+const { make, model } = car;
+
+console.log("*** EXERCISE 3 ***")
+console.log(make);
+console.log(model);
+
